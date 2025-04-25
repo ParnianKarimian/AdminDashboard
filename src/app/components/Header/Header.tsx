86db324e5c495b5336f8b2329689535a3662df34
Parnian Karimian <parniankarimian2005@gmail.com>
2025-04-25 00:43:17 -0700

    add login button

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,5 +1,19 @@
+"use client";
+
 import React from "react";
+import { useRouter } from "next/navigation";
 
 export default function Header() {
-  return <h1 className="font-bold text-white bg-zinc-900 w-full h-36 flex justify-center items-center">Header</h1>;
+  const router = useRouter();
+
+  return (
+    <div className="relative w-full h-[130px]">
+      <button
+        onClick={() => router.push("/login")}
+        className="bg-blue-600 text-white p-2 rounded justify-center items-center flex absolute right-[700px] top-10"
+      >
+        Go to Login
+      </button>
+    </div>
+  );
 }
